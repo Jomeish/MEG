9f05cb1bc5fcda54745638b2f39ea9c386dbce52
Jomeish <113467417+Jomeish@users.noreply.github.com>
2023-12-05 12:16:38 -0800

    Update index.php

--- index.php
+++ index.php
@@ -13,6 +13,33 @@
         <a href="#">Portfolio</a>
     </div>
 
+   <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Page Title</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>MEG</h1>
+        <!-- Add navigation or any header content here -->
+    </header>
+  <?php
+      echo '<p style="color: white;">This is white text.</p>';
+  ?>
+
+    <main>
+        <section>
+            <h1>Welcome to Our Website</h1>
+            <!-- Add your main content here -->
+        </section>
+    </main>
+
+    <footer>
+        <p>&copy; <?php echo date("Y"); ?> Your Website. All rights reserved.</p>
+        <!-- Add footer content or links here -->
+    </footer>
+
 </body>
 <link rel="stylesheet" type="text/css" href="style.css">
 </html>
