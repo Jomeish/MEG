1f96856de3e0ab8a52462346e1ab58ece8546a28
Jomeish <113467417+Jomeish@users.noreply.github.com>
2023-12-05 11:54:16 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
-BenrudV1
-edited on GitHub via GUI
-test 1
+<!DOCTYPE html>
+<html>
+<head>
+   
+       
+</head>
+<body>
+    <div class="navbar">
+        <a href="https://replit.com/@XAVIEROGLE/e#home.html">Home</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+        <a href="#">Services</a>
+        <a href="#">Portfolio</a>
+    </div>
+
+</body>
+<link rel="stylesheet" type="text/css" href="css.css">
+</html>
