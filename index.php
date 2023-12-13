07af98b387b70e9e0586ecba66f3a1454e7eeccd
Jomeish <113467417+Jomeish@users.noreply.github.com>
2023-12-13 11:46:55 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,42 +1,27 @@
+<?php require($_SERVER['DOCUMENT_ROOT'].'/functions.php'); ?>
 <!DOCTYPE html>
 <html>
 <head>
-   
-       
+
+
 </head>
 <body>
-    <div class="navbar">
-        <a href="https://replit.com/@XAVIEROGLE/e#home.html">Home</a>
-        <a href="#">About</a>
-        <a href="#">Contact</a>
-        <a href="#">Services</a>
-        <a href="#">Portfolio</a>
-    </div>
+
 
    <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Your Page Title</title>
+    <title>Multi Ending Game</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <header>
-        <h1>MEG</h1>
+        <h1>Multi Ending Game</h1>
         <!-- Add navigation or any header content here -->
     </header>
-  <?php
-      echo '<p style="color: white;">This is white text.</p>';
-  ?>
 
-    <main>
-        <section>
-            <h1>Welcome to Our Website</h1>
-            <!-- Add your main content here -->
-        </section>
-    </main>
 
     <footer>
-        <p>&copy; <?php echo date("Y"); ?> Your Website. All rights reserved.</p>
         <!-- Add footer content or links here -->
     </footer>
 
