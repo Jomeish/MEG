e22d66008c9cf8d4de04e689f8ff051f72d28340
Jomeish <113467417+Jomeish@users.noreply.github.com>
2023-12-13 11:45:01 -0800

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php session_start();
+
+  function nav() {
+    echo '<div class="navbar">';
+    echo '<a href="https://replit.com/@XAVIEROGLE/e#home.html">Home</a>';
+    echo '<a href="#">About</a>';
+    echo '<a href="#">Contact</a>';
+    echo '<a href="#">Services</a>';
+    echo '<a href="#">Portfolio</a>';
+    echo '</div>';
+  }
+
+
+?>
