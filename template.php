c03cea3346cb37c7a08b65f0c32384037a4ab3b9
Jomeish <113467417+Jomeish@users.noreply.github.com>
2023-12-13 11:45:23 -0800

    Create template.php

---
+++ template.php
@@ -0,0 +1,32 @@
+<?php require($_SERVER['DOCUMENT_ROOT'].'/functions.php'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+
+
+</head>
+<body>
+  <?php 
+    nav(); 
+  ?>
+
+   <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Multi Ending Game</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Template</h1>
+        <!-- Add navigation or any header content here -->
+    </header>
+
+
+    <footer>
+        <!-- Add footer content or links here -->
+    </footer>
+
+</body>
+<link rel="stylesheet" type="text/css" href="style.css">
+</html>
